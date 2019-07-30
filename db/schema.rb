8bc772180f4cc434a17216c01df8d81a742bc8a1
wipegup <wipegup@gmail.com>
2019-07-30 08:27:49 -0600

    add name to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_142124) do
+ActiveRecord::Schema.define(version: 2019_07_30_142712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_142124) do
     t.bigint "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["game_id"], name: "index_events_on_game_id"
     t.index ["sport_id"], name: "index_events_on_sport_id"
   end
