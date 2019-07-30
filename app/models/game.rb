f6e740d0973c8e338f377965569d8da2db03837a
wipegup <wipegup@gmail.com>
2019-07-30 10:27:53 -0600

    Passing competing_athletes test

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ApplicationRecord
   has_many :athletes, through: :event_participations
   has_many :sports, through: :event_participations
   has_many :events, through: :event_participations
+
+  def competing_athletes
+    athletes.distinct.count
+  end
 end
