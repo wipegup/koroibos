7370f7ce155d341d46f2658e341f920c0afc955b
wipegup <wipegup@gmail.com>
2019-07-30 11:53:06 -0600

    Passing olympian stats test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :olympians, only: [:index]
+      resources :olympian_stats, only: [:index]
     end
   end
 end
--- spec/requests/api/v1/olympian_stats/index_spec.rb
+++ spec/requests/api/v1/olympian_stats/index_spec.rb
@@ -2,27 +2,30 @@ require 'rails_helper'
 
 describe 'GET /api/v1/olympian_stats' do
   before :each do
+    game = create(:game)
     (1..4).each do |num|
-      create(:athlete,
+      athlete = create(:athlete,
         name:"Athlete#{num}", sex: (num % 2),
         height: num, weight: num, age: num
       )
+
+      create(:event_participation, athlete: athlete, game: game)
     end
   end
   it 'returns collection of statistics' do
-    get '/api/v1/olympian_stats' do
-      expect(response.status).to eq(200)
-      stats = JSON.parse(response.body, symbolize_names: true)[:olympian_stats]
+    get '/api/v1/olympian_stats'
 
-      expect(stats).to_not be(nil)
+    expect(response.status).to eq(200)
+    stats = JSON.parse(response.body, symbolize_names: true)[:olympian_stats]
+    expect(stats).to_not be(nil)
 
-      expect(stats[:total_competing_olympians]).to eq(4)
-      expect(stats[:average_age]).to eq(2.5)
+    expect(stats[:total_competing_olympians]).to eq(4)
+    expect(stats[:average_age].to_f).to eq(2.5)
 
-      weight = stats[:average_weight]
-      expect(weight[:unit]).to eq("kg")
-      expect(weight["male_olympians"]).to eq(2)
-      expect(weight["female_olympians"]).to eq(3)
-    end
+    weight = stats[:average_weight]
+    expect(weight[:unit]).to eq("kg")
+    expect(weight[:male_olympians].to_f).to eq(2)
+    expect(weight[:female_olympians].to_f).to eq(3)
+
   end
 end
