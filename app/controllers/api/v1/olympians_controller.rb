23107db195eb6c30aad80315daa4c4d8ede6444a
wipegup <wipegup@gmail.com>
2019-07-30 11:19:01 -0600

    Passing olympians by age tests

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::OlympiansController < ApplicationController
   def index
-    serialized = AthleteSerializer.new.json
-    # binding.pry
+    serialized = AthleteSerializer.new(filters).json
     render json: serialized, status: 200
+  end
+
+  private
+  def filters
+    params.permit(:age)
   end
 end
--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,12 +1,12 @@
 class AthleteSerializer
-  def initialize
-
+  def initialize(filters)
+    @filters = filters
   end
 
   def json
     {
       olympians:
-        Athlete.all.map do |athlete|
+        athletes.map do |athlete|
           {
             name: athlete.name,
             age: athlete.age,
@@ -15,7 +15,30 @@ class AthleteSerializer
             total_medals_won: athlete.total_medals,
           }
         end,
-        
+
   }
   end
+
+  private
+
+  def athletes
+    current_selection = Athlete.all
+    if @filters.keys.length != 0
+      if @filters[:age]
+        current_selection = filter_age(current_selection, @filters[:age])
+      end
+    end
+
+    current_selection
+  end
+
+  def filter_age(athletes, age_filter)
+    if age_filter == "youngest"
+      athletes = athletes.age_order(:asc, 1)
+    elsif age_filter == "oldest"
+      athletes = athletes.age_order(:desc, 1)
+    end
+    athletes
+  end
+
 end
