567fbe787a22996ddb43add9f87199f5a512c94f
wipegup <wipegup@gmail.com>
2019-07-30 11:09:45 -0600

    Add test for youngest and oldest olympians

--- spec/requests/api/v1/olympians/index_spec.rb
+++ spec/requests/api/v1/olympians/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'GET api/v1/olympians', type: :request do
 
   before :each do
-    athletes = create_list(:athlete, 5)
-    athletes.each do |athlete|
+    @athletes = create_list(:athlete, 5)
+    @athletes.each do |athlete|
       create(:event_participation, athlete: athlete)
     end
   end
@@ -24,7 +24,6 @@ describe 'GET api/v1/olympians', type: :request do
     it 'each olympian has the correct attributes' do
       get '/api/v1/olympians'
       olympians = JSON.parse(response.body, symbolize_names: true)[:olympians]
-
       olympians.each do |olympian|
         expect(olympian[:name]).not_to be(nil)
         expect(olympian[:age]).not_to be(nil)
@@ -32,6 +31,25 @@ describe 'GET api/v1/olympians', type: :request do
         expect(olympian[:sports]).not_to be(nil)
         expect(olympian[:total_medals_won]).not_to be(nil)
       end
+    end
+  end
+
+  describe 'with age query parameters' do
+    it 'can return oldest athlete' do
+      get '/api/v1/olympians?age=oldest'
+      olympians = JSON.parse(response.body, symbolize_names: true)[:olympians]
+      expect(olympians.length).to eq(1)
+      olympian = olympians[0]
+      expect(olympian[:age]).to eq(@athletes[-1].age)
+
+    end
+
+    it 'can return youngest athlete' do
+      get '/api/v1/olympians?age=youngest'
+      olympians = JSON.parse(response.body, symbolize_names: true)[:olympians]
+      expect(olympians.length).to eq(1)
+      olympian = olympians[0]
+      expect(olympian[:age]).to eq(@athletes[0].age)
     end
   end
 
