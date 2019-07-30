7eef8e4764024434af14ce079662c3d6c96dedd9
wipegup <wipegup@gmail.com>
2019-07-30 10:43:20 -0600

    Add test for olympian get

--- spec/requests/api/v1/olympians/index_spec.rb
+++ spec/requests/api/v1/olympians/index_spec.rb
@@ -9,8 +9,21 @@ describe 'GET api/v1/olympians', type: :request do
 
     it 'returns a list of olympians' do
       get '/api/v1/olympians'
-      # binding.pry
-      expect(response.body)
+      body = JSON.parse(response.body, symbolize_names: true)
+      expect(body[:olympians].class).to be(Array)
+    end
+
+    it 'each olympian has the correct attributes' do
+      get '/api/v1/olympians'
+      olympians = JSON.parse(response.body, symbolize_names: true)[:olympians]
+
+      olympians.each do |olympian|
+        expect(olympian[:name]).not_to be(nil)
+        expect(olympian[:age]).not_to be(nil)
+        expect(olympian[:team]).not_to be(nil)
+        expect(olympian[:sport]).not_to be(nil)
+        expect(olympian[:total_medals_won]).not_to be(nil)
+      end
     end
   end
 
