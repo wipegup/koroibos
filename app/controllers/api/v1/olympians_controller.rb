357d2396cecb5789da2cf3abb2e23b1d8c42f6de
wipegup <wipegup@gmail.com>
2019-07-30 10:37:44 -0600

    Passes simple olympians get test

---
+++ app/controllers/api/v1/olympians_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::OlympiansController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  
+  namespace :api do
+    namespace :v1 do
+      resources :olympians, only: [:index]
+    end
+  end
 end
